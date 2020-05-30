1f75c4431171477ac8ea9dd242bd535defb6e7f5
Devan <vanthi.svd@gmail.com>
2020-05-30 11:44:38 +0530

    href links added for all elements

--- js/style.js
+++ js/style.js
@@ -2,3 +2,18 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 ctx.fillStyle = 'green';
 ctx.fillRect(0, 0, 150, 150);
+
+function onBorderToggle() {
+  const state = document.getElementById('border-toggler').checked;
+  const allElements = document.querySelectorAll('*');
+  if (!state) {
+    for (const el of allElements) {
+      el.classList.remove('solid-border');
+    }
+    return;
+  }
+  for (const el of allElements) {
+    el.classList.add('solid-border');
+  }
+  return;
+}
